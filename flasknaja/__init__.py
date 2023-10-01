e06a434a62b4785616e9ee1d33b9c88341392788
piyapol118 <66070118@kmitl.ac.th>
2023-10-01 15:10:51 +0700

    Del old commit
    ลบ commit อันเก่า

--- flasknaja/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-def create_app():
-    app = Flask(__name__)
-
-    from .site import web
-    app.register_blueprint(web, url_prefix="/")
-
-    return app
--- flasknaja/site.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-web = Blueprint('web', __name__)
-
-@web.route("/")
-def home():
-    return render_template("index.html")
