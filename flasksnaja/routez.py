a979fc9f1abbaa02807b58888671afb8cfd1d404
NowEXs <139044432+NowEXs@users.noreply.github.com>
2023-10-01 08:21:31 +0700

    flask and folium together
    route html from folium to flask

--- flasksnaja/routez.py
+++ flasksnaja/routez.py
@@ -5,3 +5,7 @@ routes = Blueprint("routes", __name__)
 @routes.route("/")
 def home():
     return render_template("index.html")
+
+@routes.route("/map")
+def maps():
+    return render_template("routemap.html")
