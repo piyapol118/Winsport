100a0f7e5dfde4e145f85cd9c5794a451fae0858
piyapol118 <66070118@kmitl.ac.th>
2023-10-03 03:36:31 +0700

    FIrst page is done
    Nettae

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 function toggleHam(x) {
     x.classList.toggle("change");
+
+    let myMenu = document.getElementById('myMenu')
+    let winDiv = document.getElementById('winDiv')
+
+    if (myMenu.className === 'menu')  {
+        myMenu.className += ' menu-active'
+        if (myMenu.className === 'menu menu-active'){
+            winDiv.className = ' win-active'
+        }
+    } else {
+        myMenu.className = 'menu'
+        winDiv.className = 'win'
+    }
+
 }
