03d5d590d7629d68b730ba6950b166bfa52c8392
Pathosuzu <139044432+NowEXs@users.noreply.github.com>
2023-10-09 23:13:01 +0700

    Marker add
    add Mark

--- folium_map/script.py
+++ folium_map/script.py
@@ -3,17 +3,37 @@ from folium import IFrame
 import os
 import base64
 
-maps = folium.Map(location=[13.7294363,100.7785291], zoom_start=16)
+maps = folium.Map(location=[13.7294363, 100.7785291], zoom_start=16)
 route = os.path.join("routes.json")
 
-tooltip = "Click"
-html = '<img src="data:image/jpg;base64,{}">'.format
-pic1 = base64.b64encode(open('./folium_map/images/kmitl.jpg', 'rb').read()).decode()
-iframe1 = IFrame(html(pic1), width=600 + 20, height=400 + 20)
+tooltip = "FBT"
+html = """<center>
+<h2>ซุ้มวินหน้าหอFBT</h2> 
+<br/>
+</center> 
+<img src="data:image/jpg;base64,{}" style="width:75%;
+height=75%
+margin-right: 50px">
+""".format
+pic1 = base64.b64encode(open('./folium_map/images/FBT.jpg', 'rb').read()).decode()
+iframe1 = IFrame(html(pic1), width=600, height=400)
 popup1 = folium.Popup(iframe1, max_width=650)
-icon1 = folium.CustomIcon('./folium_map/images/kmitl.jpg', icon_size=(60, 60))
 
-marker1 = folium.Marker(location=[13.724705075669817, 100.7800474824299], popup=popup1, tooltip=tooltip, icon=icon1).add_to(maps)
+marker1 = folium.Marker(location=[13.722430076051083, 100.78016732950607], popup=popup1, tooltip=tooltip).add_to(maps)
+
+tooltip_en = "วินใกล้หอประชุมวิศวะ"
+html = """<center>
+<h2>วินใกล้หอประชุมวิศวะ</h2> 
+<br/>
+</center> 
+<img src="data:image/jpg;base64,{}" style="width:75%;
+height=75%
+margin-right: 50px">
+""".format
+pic2 = base64.b64encode(open('./folium_map/images/engineer1.jpg', 'rb').read()).decode()
+iframe2 = IFrame(html(pic2), width=600, height=400)
+popup2 = folium.Popup(iframe2, max_width=650)
+
+marker2 = folium.Marker(location=[13.728018637540828, 100.77791790248858], popup=popup2, tooltip=tooltip_en).add_to(maps)
 
 maps.save("./flasksnaja/templates/routemap.html")
-
