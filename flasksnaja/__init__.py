0b983c07de44fec7c5f8c64acff8fca58f8b3cb5
piyapol118 <66070118@kmitl.ac.th>
2023-11-04 03:16:12 +0700

    Login มาแล้วคร้าบ

--- flasksnaja/__init__.py
+++ flasksnaja/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -9,17 +10,23 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "alo everyone"
     app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
-
-    from .models import User, Note
-
-    create_database(app)
+    db.init_app(app)
 
     from .routez import routes
     app.register_blueprint(routes)
+
+    from .models import User, Note
     
+    with app.app_context():
+        db.create_all()
+    
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
     return app
 
-def create_database(app):
-    if not path('website/' + DB_NAME):
-        db.create_all(app=app)
-        print('Created Database!')
--- flasksnaja/routez.py
+++ flasksnaja/routez.py
@@ -1,4 +1,8 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 routes = Blueprint("routes", __name__)
 
@@ -14,29 +18,52 @@ def maps():
 def sec():
     return render_template("second_page.html")
 
-
 @routes.route("/third")
 def third():
     return render_template("third.html")
-
 
 @routes.route("/about")
 def about():
     return render_template("about_page.html")
 
+@routes.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect('/login')
+
 @routes.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("Login_page.html", boolean=True)
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('ล็อกอินสำเร็จ!', category="success")
+                login_user(user, remember=True)
+                return redirect('/sec')
+            else:
+                flash('รหัสผ่านไม่ถูกต้อง กรุณาลองอีกครั้ง', category="error")
+        else:
+            flash("ชื่อของคุณไม่มีในระบบ", category='error')
+
+    return render_template("Login_page.html", user=current_user)
 
 @routes.route("/register", methods=["GET", "POST"])
 def regist():
     if request.method == "POST":
         email = request.form.get('email')
-        firstName = request.form.get('firstName')
+        first_name = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-        if len(firstName) < 3:
+        user = User.query.filter_by(email=email).first()
+
+        if user:
+            flash('อีเมลดังกล่าวถูกใช้งานแล้ว', category='error')
+        elif len(first_name) < 3:
             flash("ชื่อผู้ใช้ต้องมีความยาวไม่ต่ำกว่า 3 ตัวอักษร", category='error')
         elif len(email) < 4 or "@" not in email:
             flash("อีเมลของคุณไม่ถูกต้อง", category="error")
@@ -45,7 +72,12 @@ def regist():
         elif len(password1) < 7:
             flash("รหัสผ่านต้องมีความยาวไม่ต่ำกว่า 7 ตัวอักษร", category='error')
         else:
+            new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(user, remember=True)
             flash("สร้างบัญชีผู้ใช้สำเร็จ", category='success')
-        
+            return redirect('/')
+
             # add user to database 
-    return render_template("register_page.html")
+    return render_template("register_page.html", user=current_user)
